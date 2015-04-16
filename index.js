84c1f736ba81032ad93512a427b4eb829adb317d
William Falk-Wallace <wgf2104@columbia.edu>
2015-04-15 21:24:09 -0700

    adds proxy server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let http = require('http')
+let destinationUrl = '127.0.0.1:8000'
 
 http.createServer((req, res) => {
   console.log(`Request received at: ${req.url}`)
@@ -6,5 +7,9 @@ http.createServer((req, res) => {
   for (let header in req.headers) {
     res.setHeader(header, req.headers[header])
   }
-
 }).listen(8000)
+
+http.createServer((req, res) => {
+  console.log(`Proxying request to: ${destinationUrl + req.url}`)
+  // Proxy code here
+}).listen(8001)
