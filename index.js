89de8143d29782a9b135c22eb7dd45d0ab3232da
William Falk-Wallace <wgf2104@columbia.edu>
2015-04-15 21:16:05 -0700

    adds header echoing

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let http = require('http')
 
 http.createServer((req, res) => {
-    console.log(`Request received at: ${req.url}`)
-    res.end('hello world\n')
+  console.log(`Request received at: ${req.url}`)
+  req.pipe(res)
+  for (let header in req.headers) {
+    res.setHeader(header, req.headers[header])
+  }
+
 }).listen(8000)
