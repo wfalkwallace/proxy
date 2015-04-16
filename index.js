b590ace31367d5a29cb24506fb21967ccd7a1133
William Falk-Wallace <wgf2104@columbia.edu>
2015-04-15 23:56:39 -0700

    adds through and tries to debug logging

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let http = require('http')
+let fs = require('fs')
 let request = require('request')
+let through = require('through')
 
 let scheme = 'http://'
 let argv = require('yargs')
@@ -8,6 +10,8 @@ let argv = require('yargs')
 let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
 let destinationUrl = argv.url || scheme + argv.host + ':' + port
 let logStream = argv.logfile ? fs.createWriteStream(argv.logfile) : process.stdout
+
+console.log(argv.logfile)
 
 http.createServer((req, res) => {
   console.log(`Request received at: ${req.url}`)
@@ -19,16 +23,16 @@ http.createServer((req, res) => {
 
 http.createServer((req, res) => {
   console.log(`Proxying request to: ${destinationUrl + req.url}`)
-  logStream.write('\n\n\n' + JSON.stringify(req.headers))
-  req.pipe(process.stdout)
+  logStream.write('\n' + JSON.stringify(req.headers))
+  through(req, logStream, { autoDestroy: false })
   let options = {
     headers: req.headers,
     method: req.method,
     url: req.headers['x-destination-url'] || `${destinationUrl}${req.url}`
   }
   let downstreamResponse = req.pipe(request(options))
-  logStream.write(JSON.stringify(downstreamResponse.headers))
-  downstreamResponse.pipe(logStream)
+  logStream.write('\n' + JSON.stringify(downstreamResponse.headers))
+  through(downstreamResponse, logStream, { autoDestroy: false })
   downstreamResponse.pipe(res)
 }).listen(8001)
 
